87830ca54e109e75a142309dd5f51f261374d20e
gil sasover <gil@sasover.huahuahua>
2020-07-03 11:41:45 +0300

    fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
 
+async function get(url) {
+    const response = await fetch(url)
+    return await response.json()
+}
+
+async function post(url, body) {
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    })
+
+    return await response.json()
+}
+
 function App() {
-  return (
-    <div>
-    </div>
-  );
+
+    React.useEffect(() => {
+    
+        post("http://localhost:8080/user/add", { firstName: "enosh", lastName: "tsur", age: 30 })
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    
+    },  [])
+
+    return (
+        <div>
+        </div>
+    );
 }
 
 export default App;
