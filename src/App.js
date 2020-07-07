b506559a4478e31defbccf9214d7c6a990c0ed8b
gil sasover <gil@sasover.huahuahua>
2020-07-07 20:56:52 +0300

    useFetch

--- src/App.js
+++ src/App.js
@@ -30,28 +30,45 @@ const responseObject = Object.freeze({
     error: null ,
 })
 
-function useGet(url) {
 
-    const [ response, setResponse, ] = React.useState({ ...responseObject })
 
-    React.useEffect(() => {
-        async function fetchData() {
-            const data = await fetch(url)
-            return await data.json()
-        } 
+// fisrt function 
+function useFetch(method) {
 
-        fetchData()
-        .then(res => setResponse({...response, data: res , loading: false}))
-        .catch(err => setResponse({...response, error: err, loading: false, }))
+    // second function 
+    function FetchData(url) {
+        const [ response, setResponse, ] = React.useState({ ...responseObject })
 
-    }, [])
+        React.useEffect(() => {
+            async function sendRequest() {
+                const data = await fetch(url, {
+                    method
+                })
+                return await data.json()
+            } 
+    
+            sendRequest()
+            .then(res => setResponse({...response, data: res , loading: false}))
+            .catch(err => setResponse({...response, error: err, loading: false, }))
+    
+        }, [])
 
-    return response
+        return response
+    }
+
+    // return second
+    return FetchData
 }
+
+const useGet = useFetch('GET')
+const useDelete = useFetch('DELETE')
+
 
 function App() {
 
-    const { data, loading, error } = useGet('http://localhost:8080/user/findById/3')
+    const { data, loading, error } = useDelete('http://localhost:8080/user/delete/3')
+    // const { data, loading, error} = usePost('http://localhost:8080/user/add', { firstName: 'itay', lastName: 'gooby', age: 23, })
+
 
     if(error) {
         console.log('!!! ', error)
